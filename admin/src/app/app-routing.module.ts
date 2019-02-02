803da20ea6f434e5f4b8b57593d8417f510af9ed
IncredibleQuark <lkupinski@gmail.com>
2019-02-03 00:38:46 +0100

    Base components structure

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PageNotFoundComponent} from "./components/common/page-not-found/page-not-found.component";
+import {WelcomeComponent} from "./components/welcome/welcome.component";
+import {MainComponent} from "./components/main/main.component";
+import {AuthGuard} from "./guards/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    canActivate: [AuthGuard],
+    pathMatch: 'full'
+  },
+  {
+    path: 'installation',
+    component: WelcomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { PageNotFoundComponent } from './components/common/page-not-found/page-not-found.component';
+import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PageNotFoundComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
